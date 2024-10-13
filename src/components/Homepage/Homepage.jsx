bb8847d1c36d8e28cda335acea0605633900a120
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-10-13 19:47:50 +0600

    About section is add

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
+import About from './About';
+import Footer from '../Shared/Footer';
 
 const Homepage = () => {
     return (
         <div>
      <Banner></Banner>
+     <About></About>
+     <Footer></Footer>
         </div>
     );
 };
